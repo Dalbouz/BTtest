12c314b4e78ad77f883feda7558402b046629651
DESKTOP-5RE5JM0\Dawud <dawud.albouz96@gmail.com>
2022-12-15 11:03:05 +0100

    Movement Logic

--- Assets/Scripts/RobberBehaviour.cs
+++ Assets/Scripts/RobberBehaviour.cs
@@ -1,26 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RobberBehaviour : MonoBehaviour
 {
     BehaviourTree tree;
+    public GameObject diamond;
+    public GameObject van;
+    NavMeshAgent agent;
+    public enum ActionState { IDLE, WORKING };
+    ActionState state = ActionState.IDLE;
 
     private void Start()
     {
+        agent = this.GetComponent<NavMeshAgent>();
         tree = new BehaviourTree();
         Node steal = new Node("Steal Something"); //kreiramo nove Nodove za svaku akciju
-        Node GoToDiamond = new Node("Go To Diamond");
-        Node GoToVan = new Node("Go To Van");
+        Leaf goToDiamond = new Leaf("Go To Diamond",GoToDiamond);
+        Leaf goToVan = new Leaf("Go To Van",GoToVan);
 
-        Node RunAway = new Node("Run Away");
-        Node FindExit = new Node("Find Exit");
-        Node GoToExit = new Node("Go To Exit");
-
+     
         //kreiramo behaviour tree
-        steal.AddChild(GoToDiamond);//krecemo od zadnje akcije s ljeve strane drveta i krecemo se u desno po istoj ravnini
-        steal.AddChild(GoToVan);
+        steal.AddChild(goToDiamond);//krecemo od zadnje akcije s ljeve strane drveta i krecemo se u desno po istoj ravnini
+        steal.AddChild(goToVan);
         tree.AddChild(steal);//nakraju u sami tree dodajemo Steal Node
+
+        tree.Process();//ovo ce pokrenuti BT od ovoj objekta
+
         tree.PrintTree();
+    }
+
+    public Node.Status GoToDiamond()
+    {
+        return GoToLocation(diamond.transform.position);
+    }
+    public Node.Status GoToVan()
+    {
+        return GoToLocation(van.transform.position);
+    }
+
+    Node.Status GoToLocation(Vector3 destination)
+    {
+        float distanceToTaret = Vector3.Distance(destination, this.transform.position);
+        if(state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }
+        else if(Vector3.Distance(agent.pathEndPosition,destination)>=2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }
+        else if (distanceToTaret < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCCESS;
+        }
+        return Node.Status.RUNNING;
     }
 }
